43dd3dd8da10162925fa0340b79fe32c0ed40fdf
Jurriaan Pruis <email@jurriaanpruis.nl>
2015-05-11 13:00:20 +0200

    Add two additional simulations (comparisons with real experiments)

---
+++ src/event3.cpp
@@ -0,0 +1,64 @@
+#include "event_based/types.h"
+#include "event_based/event_component.h"
+#include "event_based/observer.h"
+#include "event_based/count_observer.h"
+#include "event_based/connect_observer.h"
+#include "event_based/phase_shifter.h"
+#include "event_based/neutron_beam_splitter.h"
+#include <boost/progress.hpp>
+
+using namespace event_based;
+using namespace std;
+const size_t runs = 700;
+const size_t repeats = 71517;
+const double PI = 3.141592653589793238463;
+const double alpha = 0.73;
+const double reflection = 0.2210;
+const Message input = {{0.5,0.5},{0.5,0.5}};
+
+
+int main ()
+{
+  std::random_device rd;
+  std::mt19937_64 generator(rd());
+  std::uniform_real_distribution<double> distribution(45.5, 62.5);
+  NeutronBeamSplitter bs0(alpha, reflection);
+  NeutronBeamSplitter bs1(alpha, reflection);
+  NeutronBeamSplitter bs2(alpha, reflection);
+  NeutronBeamSplitter bs3(alpha, reflection);
+  PhaseShifter xi0;
+  PhaseShifter xi1;
+  CountObserver obs1(bs3, 0);
+  CountObserver obs2(bs3, 1);
+
+  auto test  = bs0(0) + bs1(0);
+  auto test1 = bs1(1) + xi0(0);
+  auto test2 = xi0(0) + bs3(1);
+
+  auto test3 = bs0(1) + bs2(1);
+  auto test4 = bs2(0) + xi1(0);
+  auto test5 = xi1(0) + bs3(0);
+
+  boost::progress_display progress(runs, std::cerr);
+
+  for (size_t i = 0; i < runs; ++i)
+  {
+    auto angle = distribution(generator);
+    xi0.set_phase(angle);
+    xi1.set_phase(0);
+
+    Message message;
+    for (size_t j = 0; j < repeats; ++j)
+    {
+      message = input;
+      bs0.process(0, message);
+    }
+
+    std::cout << angle << ", " << obs1.count() << ", " << obs2.count() << std::endl;
+    obs1.reset();
+    obs2.reset();
+    ++progress;
+  }
+
+  cerr << "done\n";
+}
